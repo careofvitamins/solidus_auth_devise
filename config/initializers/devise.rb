85958caa08b7c58268b5409a557109f2bafbbbef
Eric Gross <github@ericgross.com>
2016-09-29 11:49:11 -0400

    Handle requirements for invitations

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -314,4 +314,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  config.require_password_on_accepting = false
 end
--- lib/controllers/frontend/spree/user_invitations_controller.rb
+++ lib/controllers/frontend/spree/user_invitations_controller.rb
@@ -1,2 +1,20 @@
 class Spree::UserInvitationsController < Devise::InvitationsController
+  skip_before_filter :authenticate_inviter!, :only => [:new, :create]
+  skip_before_filter :has_invitations_left?, :only => [:create]
+
+  def edit
+    sign_in resource
+    redirect_to spree.new_spree_user_invitation_path
+  end
+
+  protected
+  def authenticate_inviter!
+    inviter = send(:"authenticate_#{resource_name}!")
+    return inviter if inviter
+
+    Spree::User.find_or_create_by!(email: Figaro.env.email_from_address!) do |user|
+      user.password = 'P8FA9sEXzs'
+    end
+  end
+
 end
